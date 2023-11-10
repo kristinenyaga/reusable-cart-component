7023c53fbdce8d6a27ab5b9982390fb0db74b17a
krisine <kristine.nyaga@student.moringaschool.com>
2023-11-10 14:24:59 +0300

    testing the package

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,47 @@
 import React from 'react'
-
-import { ExampleComponent } from 'reusable-cart-component'
+import ReusableCartComponent from 'reusable-cart-component'
 import 'reusable-cart-component/dist/index.css'
-
+const items = [
+  {
+    id: 1,
+    name: 'Product A',
+    price: 20,
+    quantity: 1,
+    description: 'Description of Product A',
+    imageUrl:
+      'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: 2,
+    name: 'Product B',
+    price: 15,
+    quantity: 1,
+    description: 'Description of Product B',
+    imageUrl:
+      'https://images.unsplash.com/photo-1576566588028-4147f3842f27?auto=format&fit=crop&q=80&w=1964&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: 2,
+    name: 'Product C',
+    price: 10,
+    quantity: 1,
+    description: 'Description of Product C',
+    imageUrl:
+      'https://images.unsplash.com/photo-1596755094514-f87e34085b2c?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2hpcnRzfGVufDB8fDB8fHww'
+  },
+  {
+    id: 4,
+    name: 'Product D',
+    price: 14,
+    quantity: 1,
+    description: 'Description of Product D',
+    imageUrl:
+      'https://images.unsplash.com/photo-1618354691373-d851c5c3a990?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c2hpcnR8ZW58MHx8MHx8fDA%3D'
+  }
+  // Add more items as needed
+]
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ReusableCartComponent items={items} />
 }
 
 export default App
