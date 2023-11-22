b05508dd0d8937acb1fdd2ad03ca169e32537746
krisine <kristine.nyaga@student.moringaschool.com>
2023-11-22 05:42:42 +0300

    component files

--- README.md
+++ README.md
@@ -1,30 +1,58 @@
-# reusable-cart-component
+# Reusable-Cart-Component
 
-> This is a reusable cart component you can install to you react application to display cart items. it includes all functionalities of a cart component: adding or reducing the quantity of an item, removing items from the cart as well as clearing the cart.
+> This is a reusable cart component you can install to an e-commerce react application to display cart items. it includes all functionalities of a cart component: 
+1. Viewing items added to cart
+2. Adding or reducing the quantity of an item
+3. Removing an item from the cart
+4. Clearing the cart.
+5. Displaying the total amount of all items in Ksh
+6. Displaying the amount per item based on the quantity
 
 [![NPM](https://img.shields.io/npm/v/reusable-cart-component.svg)](https://www.npmjs.com/package/reusable-cart-component) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
-## Install
+## How to Install
 
 ```bash
 npm install --save reusable-cart-component
 ```
 
 ## Usage
+- In the jsx component you want to display the cart items you need to:
+1. Install the components in your main folder
+2. Ensure the component is visible in your `package.json file`
+3. Import the component as shown below
+4. Instantiate the component
+5. Provide the cart items (the items selected by a user from the main products page)
+6. The component will do the rest 🎊
 
 ```jsx
-import React, { Component } from 'react'
-
 import MyComponent from 'reusable-cart-component'
 import 'reusable-cart-component/dist/index.css'
 
-class Example extends Component {
-  render() {
-    return <MyComponent />
-  }
+
+const Cart = () => {
+  return (
+    <div>
+      <ReusableCartComponent items={items} cartId={cartId}  />  
+    </div>
+  )
 }
+
+export default Cart
+```
+## Requirements
+```
+Node version >=10
 ```
 
 ## License
 
-MIT © [kristinenyaga](https://github.com/kristinenyaga)
+MIT © 
+## Github
+[kristinenyaga](https://github.com/kristinenyaga)
+
+## Reach out
+- For any queries,suggestions reach out to:
+nyagakristine@gmail.com
+angieschacha@gmail.com
+emutheu1998@gmail.com
--- src/ReusableCartComponent.js
+++ src/ReusableCartComponent.js
@@ -1,41 +1,94 @@
 import React from 'react'
 import './styles.module.css'
+import axios from 'axios'
+const ReusableCartComponent = ({ items, cartId }) => {
+  const [cartItems, setCartItems] = React.useState([])
+  React.useEffect(() => {
+    if (cartItems.length === 0) {
+      setCartItems(items)
+    } else {
+      setCartItems(cartItems)
+    }
+  }, [cartItems, items])
 
-const ReusableCartComponent = ({ items }) => {
-  const [cartItems, setCartItems] = React.useState(items)
-  const updateQuantity = (index, quantity) => {
-    // Create a copy of the cartItems array and update the quantity of the specified item
-    const updatedCart = [...cartItems]
-    // Ensure the quantity doesn't go below zero
-    updatedCart[index].quantity = Math.max(quantity, 1)
-    setCartItems(updatedCart)
-  }
-
-  // Calculate the total price of all items in the cart
   const calculateTotalPrice = () => {
     return cartItems.reduce(
-      (total, item) => total + item.price * item.quantity,
+      (total, item) => total + item.product.unit_price * item.quantity,
       0
     )
   }
-  const removeItem = (id) => {
-    // Create a copy of the cartItems array and remove the specified item
-    const updatedCart = cartItems.filter((item) => item.id !== id)
+  const updateQuantity = async (productId, newQuantity) => {
+    // Ensure the new quantity is at least 1
+    const validatedQuantity = Math.max(newQuantity, 1)
 
-    setCartItems(updatedCart)
+    const updatedItems = cartItems.map((item) => {
+      if (item.id === productId) {
+        return { ...item, quantity: validatedQuantity }
+      }
+      return item
+    })
+    axios
+      .patch(
+        `https://shoplify-prod-231a968d6096.herokuapp.com/shop/carts/${cartId}/items/${productId}/`,
+        {
+          quantity: validatedQuantity
+        }
+      )
+      .then((response) => {
+        setCartItems(updatedItems)
+        window.alert('item updated successfully')
+      })
+      .catch((error) => {
+        // Handle error
+        console.error('Error removing item:', error)
+      })
   }
 
+  const removeItem = (productId) => {
+    const updatedItems = cartItems.filter((item) => item.id !== productId)
+    setCartItems(updatedItems)
+
+    // Make a DELETE request to remove the specific item from the cart
+    axios
+      .delete(
+        `https://shoplify-prod-231a968d6096.herokuapp.com/shop/carts/${cartId}/items/${productId}/`
+      )
+      .then((response) => {
+        setCartItems(updatedItems)
+        window.alert('item deleted successfully')
+      })
+      .catch((error) => {
+        // Handle error
+        console.error('Error removing item:', error)
+      })
+  }
+  const clearCart = () => {
+    axios
+      .delete(
+        `https://shoplify-prod-231a968d6096.herokuapp.com/shop/carts/${cartId}/`
+      )
+      .then((response) => {
+        window.alert('cart cleared successfully')
+        setCartItems([])
+        window.localStorage.removeItem('cartId')
+      })
+      .catch((error) => {
+        // Handle error
+        console.error('Error clearing cart:', error)
+      })
+  }
   return (
     <div className='container'>
       <p className='text-center fs-3'>Here are the items on your cart</p>
       <div className='total-price d-flex justify-content-between align-items-center'>
-        <button className='btn btn-md btn-success'>Place Order</button>
         <p className='fs-3'>Total Price: ksh {calculateTotalPrice()}</p>
-        <button className='btn btn-md btn-danger'>Clear Cart</button>
+        <button className='btn btn-md btn-danger' onClick={clearCart}>
+          Clear Cart
+        </button>
       </div>
       <div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3'>
         {cartItems.map((item, index) => (
-          <div className='col' key={index}>
+          <div className='col' key={item.id}>
             <div className='card shadow-sm image-container'>
               <img
                 className='bd-placeholder-img card-img-top'
@@ -47,7 +100,10 @@ const ReusableCartComponent = ({ items }) => {
               <div className='overlay' />
 
               <div className='card-body'>
-                <p className='card-text text-center'>{item.name}</p>
+                <p className='card-text text-center'>{item.product.title}</p>
+                <p className='card-text text-center'>
+                  {item.product.description}
+                </p>
                 <div className='d-flex justify-content-between align-items-center'>
                   <div className='btn-group'>
                     <button
@@ -62,20 +118,26 @@ const ReusableCartComponent = ({ items }) => {
                     <div className='d-flex justify-content-between align-items-center'>
                       <button
                         type='button'
-                        onClick={() => updateQuantity(index, item.quantity + 1)}
+                        onClick={() =>
+                          updateQuantity(item.id, item.quantity + 1)
+                        }
                         className='btn btn-md btn-outline-primary'
                       >
                         +
                       </button>
                       <p className='fs-1 p-2 mt-3'>{item.quantity}</p>
                       <button
-                        onClick={() => updateQuantity(index, item.quantity - 1)}
+                        onClick={() =>
+                          updateQuantity(item.id, item.quantity - 1)
+                        }
                         className='btn btn-md btn-outline-danger'
                       >
                         -
                       </button>
                     </div>
-                    <p className='fs-4'>ksh {item.price * item.quantity}</p>
+                    <p className='fs-4'>
+                      ksh {item.product.unit_price * item.quantity}
+                    </p>
                   </div>
                 </div>
               </div>
